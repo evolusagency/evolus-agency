5c875f85f78785f8244f519111d678a1912096da
evolusagency <evolusagency@gmail.com>
2026-06-09 10:07:29 +0200

    static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: 'https://evolus.agency',
 
-  output: 'server',
+  output: 'static',
 
   adapter: cloudflare({
     mode: 'advanced'
