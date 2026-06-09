af1e414d140d578388ede2bf405c97fc2c473600
evolusagency <evolusagency@gmail.com>
2026-06-09 10:41:28 +0200

    update v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,23 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import { unified } from '@astrojs/markdown-remark';
-import rehypeInternalLinks from './src/lib/rehypeInternalLinks.ts';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://evolus.agency',
-
-
+  output: 'server',
+  adapter: cloudflare(),
+  trailingSlash: 'ignore',
+  compressHTML: true,
+  build: {
+    minifyCSS: true,
+    minifyJS: true,
+  },
 
   integrations: [
-    sitemap()
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
   ],
-
-  markdown: {
-    processor: unified({
-      rehypePlugins: [
-        rehypeInternalLinks,
-      ],
-    }),
-  },
 });
