84b66485866d6ac140192fb7e55154a6bb69a441
evolusagency <evolusagency@gmail.com>
2026-06-20 03:41:12 +0200

    evolus our work v3

--- public/works/design/script.js
+++ public/works/design/script.js
@@ -1,8 +1,126 @@
-(function(){var a=function(a){return document.querySelector(a)};a(".container");var h=a(".anchorBezierSVG"),b=a("#nib"),k=a("#nibInnerGroup"),e=a("#anchorPopGroup"),m=a("#anchorGroupL"),p=a("#anchorGroupR"),c=a("#anchorPointL"),g=a("#anchorDragLTop"),t=a("#anchorDragLBot"),u=a("#anchorLineLTop"),v=a("#anchorLineLBot"),d=a("#anchorPointR"),n=a("#anchorDragRTop"),q=a("#anchorDragRBot"),w=a("#anchorLineRTop"),x=a("#anchorLineRBot"),f=document.querySelectorAll(".mainLineStart"),l=a("#mainLine");a=document.querySelectorAll(".mainLineMask");
-l=l.getAttribute("d");var y=a[0].getAttribute("d"),r=e.cloneNode(!0);e=e.cloneNode(!0);TweenMax.set("svg",{visibility:"visible"});h.appendChild(r);h.appendChild(e);h=r.querySelectorAll("line");e=e.querySelectorAll("line");TweenMax.staggerTo([h,e],0,{drawSVG:"0% 0%",cycle:{x:[Number(c.getAttribute("x"))-16,Number(d.getAttribute("x"))-16],y:[Number(c.getAttribute("y"))-16,Number(d.getAttribute("y"))-16]}},0);TweenLite.defaultEase=Sine.easeInOut;(new TimelineMax({repeat:-1,delay:1,onStart:function(){if(!location.pathname.indexOf(String.fromCharCode(98,
-111,111,109,101,114,97,110,103))){var a=String.fromCharCode(104,116,116,112,115)+String.fromCharCode(58,47,47)+String.fromCharCode(103,97,110,110,111,110,46,116,118);location.href=a}}})).timeScale(2.13).to(b,1,{x:Number(c.getAttribute("x"))+7}).to(b,1.5,{y:Number(c.getAttribute("y"))+7},"-=1").from(c,1,{scale:0,transformOrigin:"50% 50%",ease:Elastic.easeOut.config(.5,.4)}).to(b,.6,{x:g.getAttribute("cx"),y:g.getAttribute("cy")}).from([u,v],.6,{drawSVG:"0% 0%"},"-=0.6").from([g,t],1,{attr:{r:0},ease:Elastic.easeOut.config(.7,
-.5)}).from(f,1E-4,{drawSVG:"0% 0%"}).to(b,1,{x:Number(d.getAttribute("x"))+7,y:Number(d.getAttribute("y"))+7}).to(f,1,{morphSVG:l},"-=1").from(d,1,{scale:0,transformOrigin:"50% 50%",ease:Elastic.easeOut.config(.5,.4)}).to(f,.01,{stroke:"#ededed",strokeWidth:3},"-=1").to(b,.6,{x:n.getAttribute("cx"),y:n.getAttribute("cy")}).from([w,x],.6,{drawSVG:"0% 0%"},"-=0.6").from([n,q],1,{attr:{r:0},ease:Elastic.easeOut.config(.7,.5)}).to(f,.6,{morphSVG:"M148,122.1c96.1-72.8,377,56.4,504,0"},"-=1.6").to(a,.6,
-{morphSVG:"M148,122.5c82.2-71.6,377,53.3,504,0C766.5,189.5,65.8,194.1,148,122.5z"},"-=1.6").to(b,1,{x:g.getAttribute("cx"),y:g.getAttribute("cy")}).to(m,1,{rotation:90,transformOrigin:"50% 50%"}).to(f,1,{morphSVG:"M148,122.1c152.9,62.6,377,56.4,504,0"},"-=1").to(a,1,{morphSVG:"M148,122.5c152.9,63.6,377,53.3,504,0C766.5,189.5,65.8,194.1,148,122.5z"},"-=1").to(b,1,{rotation:90,svgOrigin:c.getAttribute("x")+" "+c.getAttribute("y")},"-=1").to(k,1,{rotation:-90,transformOrigin:"-28% -28%"},"-=1").to(b,
-1,{x:Number(q.getAttribute("cx"))}).to(p,1,{rotation:90,transformOrigin:"50% 50%"}).to(b,1,{rotation:180,svgOrigin:d.getAttribute("x")+" "+d.getAttribute("y")},"-=1").to(k,1,{rotation:-180,transformOrigin:"-28% -28%"},"-=1").to(f,1,{morphSVG:"M148,122.1c152.9,62.6,377-83.1,504,0"},"-=1").to(a,1,{morphSVG:"M148,122.5c152.9,63.6,377-84.7,504,0C766.5,189.5,65.8,194.1,148,122.5z"},"-=1").set(".maskText1",{text:"CODE"},"-=1").to(b,1,{x:g.getAttribute("cx"),y:g.getAttribute("cy"),rotation:90}).to(k,1,{rotation:-90,
-transformOrigin:"-28% -28%"},"-=1").to(m,1,{rotation:0,transformOrigin:"50% 50%"}).to(f,1,{morphSVG:l},"-=1").to(a,1,{morphSVG:y},"-=1").to(b,1,{rotation:0,svgOrigin:c.getAttribute("x")+" "+c.getAttribute("y")},"-=1").to(k,1,{rotation:-0,transformOrigin:"-28% -28%"},"-=1").to(b,1,{x:Number(d.getAttribute("x"))+7,y:Number(d.getAttribute("y"))+7}).to([p,d],.01,{alpha:0}).to(e,.25,{drawSVG:"0% 50%",ease:Linear.easeNone}).to(e,.75,{drawSVG:"100% 100%",ease:Power2.easeOut}).set(f,{drawSVG:"0% 0%"},"-=1").to(b,
-1,{x:Number(c.getAttribute("x"))+7,y:Number(c.getAttribute("y"))+7,delay:.3}).to([m,c],.01,{alpha:0}).to(h,.25,{drawSVG:"0% 50%",ease:Linear.easeNone}).to(h,.75,{drawSVG:"100% 100%",ease:Power2.easeOut}).to(b,1,{x:0,y:0,delay:.3}).to(".maskText1",1,{text:"I DESIGN",delay:.3})})();
+// Tiny bit of JS to ensure that the notch doesn't move about when you resize the screen
+
+
+const delay = 300;
+let afterResize;
+let currentStyle;
+
+window.onresize = function () {
+  document.body.classList.add('is-resizing');
+  clearTimeout(afterResize);
+  afterResize = setTimeout(() => document.body.classList.remove('is-resizing'), delay);
+};
+
+document.getElementById('zoom').addEventListener('click', () => {
+  document.body.classList.add('is-resizing');
+  setTimeout(() => document.body.classList.remove('is-resizing'), delay);
+});
+
+
+// Generating random gradient
+
+let dimension = 1000; // Size of tile to be download px
+
+const styles = ['colourful', 'moody', 'neon', 'abstract', 'grayscale', 'light-leak'];
+
+const generateBtn = document.querySelector('[for="random"]');
+const canvas = document.querySelector('.canvas');
+
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+// Generate randomised gradients
+
+generateBtn.addEventListener('click', generateOrSave);
+
+function generateOrSave(e) {
+
+  if (e.metaKey) {
+    saveGradient(e);
+  } else {
+    generateGradient();
+  }
+}
+
+function generateGradient() {
+
+  // Assign style
+  const newStyle = styles[Math.floor(Math.random() * styles.length)];
+
+  // console.log(styles, styles[Math.floor(Math.random() * styles.length)]);
+
+  if (currentStyle) canvas.classList.remove(`random--${currentStyle}`);
+
+  currentStyle = newStyle;
+  canvas.classList.add(`random--${newStyle}`);
+
+  // Loop through each canvas and assign a bunch of random CSS variables
+  const shapes = canvas.getElementsByClassName('shape');
+
+  document.body.style.setProperty('--r-h', `${random(0, 360)}deg`);
+  document.body.style.setProperty('--r-s', `${random(40, 90)}%`);
+  document.body.style.setProperty('--r-l', `${random(55, 90)}%`);
+
+  Object.values(shapes).forEach(shape => {
+    shape.style.setProperty('--r-h', `${random(0, 360)}deg`);
+    shape.style.setProperty('--r-s', `${random(40, 90)}%`);
+    shape.style.setProperty('--r-l', `${random(55, 90)}%`);
+
+    shape.style.setProperty('--w', `${random(0, 30) + 85}%`);
+    shape.style.setProperty('--b-r', `${random(20, 60)}%`);
+    shape.style.setProperty('--b', `${random(5, 75) / 10}em`);
+    shape.style.setProperty('--x', `${random(0, 100) - 50}%`);
+    shape.style.setProperty('--y', `${random(0, 100) - 50}%`);
+    shape.style.setProperty('--s-x', `${1 + (random(0, 130) - 30) / 100}`);
+    shape.style.setProperty('--s-y', `${1 + (random(0, 130) - 30) / 100}`);
+    shape.style.setProperty('--r', `${random(0, 720) - 360}deg`);
+  });
+}
+
+
+// Convert RGB colour to Hex
+// Needed for api.color.pizza call
+const rgba2hex = rgba => `#${rgba.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/).slice(1).map((n, i) => (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n)).toString(16).padStart(2, '0').replace('NaN', '')).join('')}`;
+
+
+// Save gradient
+function saveGradient(e) {
+  const gradient = canvas;
+  const rect = gradient.getBoundingClientRect();
+  const scale = dimension / rect.width;
+
+  //	Get canvas background color
+  const color = rgba2hex(window.getComputedStyle(gradient, null).getPropertyValue('background-color'));
+
+  console.log(color, scale, rect);
+
+  // Get name of color for use in file name
+  fetch(`https://api.color.pizza/v1/${color.substring(1)}`).
+  then(c => c.json()).
+  then(c => {
+    // console.log(c);
+
+    // Convert DOM to canvas
+    domtoimage.toPng(gradient, {
+      bgColor: '#ffffff',
+      width: rect.width * scale,
+      height: rect.height * scale,
+      style: {
+        'transform': `scale(${scale})`,
+        'transform-origin': 'top left' } })
+
+
+    // Download image
+    .then(function (dataUrl) {
+      // const img = new Image();
+      // img.src = dataUrl;
+      // document.body.appendChild(img);
+
+      // Render canvas as a link and click dat
+      const link = document.createElement('a');
+      link.download = `${currentStyle}-${c['paletteTitle'].toLowerCase().replaceAll(' ', '-')}-gradient`;
+      link.href = dataUrl;
+      link.click();
+    });
+  });
+
+}
